eecd1660950dea4a777f41627c84872c50e0cd44
Subarctic Blast <unabashedfeline@gmail.com>
2024-11-26 00:43:46 +0300

    Adds preliminary_course theme_1 lesson_5 exercise_1 solution

---
+++ preliminary_course/theme_1/lesson_5/exercise_1.cpp
@@ -0,0 +1,20 @@
+/* Урок 5: 
+ *
+ * Задание 1
+ *
+ * Исправьте ошибки в программе, которая дана в заготовке кода.
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "The quote from \"Hamlet\":"s << endl;
+    cout << "To be, or not to be, that is the question:"s << endl;
+    cout << "Whether 'tis nobler in the mind to suffer" << endl;
+    cout << "The slings and arrows of outrageous fortune,"s << endl;
+    cout << "Or to take arms against a sea of troubles"s << endl;
+    cout << "And by opposing end them."s << endl;
+    return 0;
+}

37b3b47c112713c28334d9099dbc8cfe8ed9e415
Subarctic Blast <unabashedfeline@gmail.com>
2025-01-05 22:02:22 +0300

    Adds preliminary_course theme_3 lesson_6 exercise_1 solution

---
+++ preliminary_course/theme_3/lesson_6/exercise_1.cpp
@@ -0,0 +1,21 @@
+/* Урок 6:
+ *
+ * Задание 1
+ *
+ * Выведите последний элемент вектора some_vector либо строку "empty", если вектор пуст
+ */
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    std::vector<int> some_vector = {1, 2, 3};
+
+    if (!some_vector.empty()) {
+        std::cout << some_vector[some_vector.size() - 1] << std::endl;
+    } else {
+        std::cout << "empty"s << std::endl;
+    }
+}
